2cd3582e7dd6102ab93f898538c1835e86b59be7
Imamachi-n <naoto.imamachi@gmail.com>
2021-05-06 23:00:16 +0900

    feat: 余計な文字をカットする関数を追加

--- src/packages/cleanText.py
+++ src/packages/cleanText.py
@@ -2,6 +2,14 @@ import re
 
 
 def clean_text(text):
+    """自然言語処理を行う上で余計な文字を削除する
+
+    Args:
+        text (string): 文字列
+
+    Returns:
+        [string]: 前処理済みの文字列
+    """
     # 改行コードをあわせる & 両端の空白を除く
     replaced_text = '\n'.join(str.strip()
                               for str in text.splitlines() if str != "")
@@ -9,7 +17,7 @@ def clean_text(text):
     replaced_text = re.sub(r'[（）()]', ' ', replaced_text)     # （）の除去
     replaced_text = re.sub(r'[［］\[\]]', ' ', replaced_text)   # ［］の除去
     replaced_text = re.sub(r'[@＠]\w+', '', replaced_text)  # メンションの除去
-    # URLの除去
-    replaced_text = re.sub(r'https?:\/\/.*?[\r\n ]', '', replaced_text)
+    replaced_text = re.sub(
+        r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', '', replaced_text)  # URLの除去
     replaced_text = re.sub(r'　', ' ', replaced_text)  # 全角空白の除去
     return replaced_text
---
+++ src/tests/cleanText_test.py
@@ -0,0 +1,37 @@
+import unittest
+
+from src.packages import cleanText
+
+
+class TestCleanText(unittest.TestCase):
+
+    def test_clean_text_1(self):
+        actual = cleanText.clean_text(" てすと\r\nテスト　")
+        self.assertEqual("てすと\nテスト", actual)
+
+        actual = cleanText.clean_text("【てすと】 テスト")
+        self.assertEqual(" てすと  テスト", actual)
+
+        actual = cleanText.clean_text("（てすと） テスト")
+        self.assertEqual(" てすと  テスト", actual)
+
+        actual = cleanText.clean_text("(てすと) テスト")
+        self.assertEqual(" てすと  テスト", actual)
+
+        actual = cleanText.clean_text("[てすと] テスト")
+        self.assertEqual(" てすと  テスト", actual)
+
+        actual = cleanText.clean_text("@てすと テスト")
+        self.assertEqual(" テスト", actual)
+
+        actual = cleanText.clean_text("＠てすと テスト")
+        self.assertEqual(" テスト", actual)
+
+        actual = cleanText.clean_text("てすと http://example.com test")
+        self.assertEqual("てすと  test", actual)
+
+        actual = cleanText.clean_text("てすと https://example.com test")
+        self.assertEqual("てすと  test", actual)
+
+        actual = cleanText.clean_text("てすと https://example.com")
+        self.assertEqual("てすと ", actual)
